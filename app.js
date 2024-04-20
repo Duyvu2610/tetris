b738f39e2ccdd8135e81be6b88386e301b9ce9e5
Vund <vund1@runsystem.net>
2024-04-20 19:29:28 +0700

    create version 2

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
-
 window.onload = () => {
+  // nhận các phần tử DOM
   const grid = document.querySelector('.grid')
-  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const miniGrid = document.querySelector('.mini-grid');
+  const endElement = document.querySelector('.end')
+  const newGame = document.querySelector('.new-game-button')
+  const levelButtons = document.querySelectorAll('.level-button');
+  const replayButton = document.querySelector('.replay-button');
+  const volumeControl = document.getElementById('volume');
+  const volumeValue = document.querySelector('#volume-value');
   const scoreDisplay = document.querySelector('#score')
   const startBtn = document.querySelector('#start-button')
-  const width = 10
+  const displaySquares = document.querySelectorAll('.mini-grid div')
+
+  // biến let
+  let level = 1;
+  let squares = [];
   let nextRandom = 0
   let timerId
   let score = 0
+  let obstacles = [];
+  let currentPosition = 4
+  let currentRotation = ROTATE_0DEG
+  let randomNumber = Math.floor(Math.random()*pieces.length)
+  let currentPiece = pieces[randomNumber][currentRotation]
+
+  // biến const
+  const keyMap = {
+    37: moveLeft,  // Left arrow key
+    38: rotate,    // Up arrow key
+    39: moveRight, // Right arrow key
+    40: moveDown   // Down arrow key
+  };
+  const rowNumber = 10
+  const time = 500
   const colors = [
     'orange',
     'red',
@@ -15,8 +40,6 @@ window.onload = () => {
     'green',
     'blue'
   ]
-
-  // Piece
   const lPiece = [
     [
       [0, 0, 1],
@@ -38,7 +61,7 @@ window.onload = () => {
       [0, 1, 0],
       [0, 1, 0]
     ]
-  ]
+  ];
   const zPiece = [
     [
       [1, 1, 0],
@@ -129,91 +152,214 @@ window.onload = () => {
     ]
 
   ];
-
   const pieces = [lPiece, zPiece, tPiece, oPiece, iPiece]
-
-  let currentPosition = 4
-  let currentRotation = 0
-
-  // rotate
   const ROTATE_0DEG = 0;
-  const ROTATE_90DEG = 1;
-  const ROTATE_180DEG = 2;
-  const ROTATE_270DEG = 3;
+  const displayWidth = 4
+  const displayIndex = 0
 
-  // chọn ngẫu nhiên một piece và hướng xoay đầu tiên
-  let randomNumber = Math.floor(Math.random()*pieces.length)
-  let currentPiece = pieces[randomNumber][ROTATE_0DEG]
+  // Event listeners
+  replayButton.addEventListener('click', () => {
+    resetGameState();
+  })
+  levelButtons.forEach(button => {
+    changeLevelBtnColor();
+    button.addEventListener('click', function() {
+
+      level = this.value;
+      changeLevelBtnColor();
+    });
+  });
+  volumeControl.addEventListener('input', function() {
+    // Assuming you have an audio element with the id 'audio'
+    const audio = document.querySelector('#audio');
+    // audio.volume = this.value;
+
+    // Update the displayed volume value
+    volumeValue.textContent = `${Math.round(this.value * 100)}%`;
+  });
+  newGame.addEventListener('click', () => {
+    endElement.style.display = 'none'
+    window.location.reload()
+  })
+  startBtn.addEventListener('click', () => {
+    console.log(level)
+    if (timerId) {
+      clearInterval(timerId)
+      timerId = null
+    } else {
+      draw()
+      if (level === '2') {
+        obstacles = [142, 143, 147, 148];
+      }
+      obstacles.forEach(index => {
+        squares[index].classList.add('taken');
+        squares[index].style.backgroundColor = 'black';
+      });
+      timerId = setInterval(moveDown, time)
+      nextRandom = Math.floor(Math.random() * pieces.length)
+      displayShape()
+    }
+  })
+  document.addEventListener('keyup', control)
+
+  // functions
+  function resetGameState() {
+    // Reset game state
+    squares.forEach(square => {
+      square.classList.remove('piece', 'taken', 'obstacle');
+      square.style.backgroundColor = '';
+      square.style.boxShadow = '';
+
+    });
+    // Reset score
+    score = 0;
+
+    scoreDisplay.innerHTML = score;
+    // Reset piece position and rotation
+    currentPosition = 4;
+
+    currentRotation = ROTATE_0DEG;
+    // generateObstacles();
+    if (level === '2') {
+      obstacles = [142, 143, 147, 148];
+    }
+    obstacles.forEach(index => {
+      squares[index].classList.add('taken');
+      squares[index].style.backgroundColor = 'black';
+
+    });
+    // Generate new piece
+    randomNumber = Math.floor(Math.random() * pieces.length);
+
+    currentPiece = pieces[randomNumber][currentRotation];
+    // Draw new piece
+
+    draw();
+    // Start game
+    if (timerId) {
+      clearInterval(timerId);
+    }
+
+    timerId = setInterval(moveDown, time);
+    // Display next piece
+    nextRandom = Math.floor(Math.random() * pieces.length);
+
+    displayShape();
+    // Add event listener for keyboard controls
+
+    document.addEventListener('keyup', control)
+    // Hide game over screen
+    endElement.style.display = 'none';
+
+  }
+
+  // Create main grid and taken blocks in one loop
+  for (let i = 0; i < 210; i++) { // 200 cells for the grid + 10 taken cells
+    const div = document.createElement('div');
+    div.classList.add('cell');
+    if (i >= 200) div.classList.add('block'); // Last 10 cells are taken
+    grid.appendChild(div);
+    squares.push(div); // Add the created div to the squares array
+  }
+
+  // Tạo lưới phụ (mini-grid) với 16 ô
+  for (let i = 0; i < 16; i++) {
+    const div = document.createElement('div');
+    miniGrid.appendChild(div);
+  }
 
   // draw the Piece
   function draw() {
-    console.log(currentPiece)
     currentPiece.forEach((row, y) => {
       row.forEach((value, x) => {
         if (value !== 0) {
-          squares[currentPosition + y * width + x].classList.add('tetromino');
-          squares[currentPosition + y * width + x].style.backgroundColor = colors[randomNumber];
+          squares[currentPosition + y * rowNumber + x].classList.add('piece');
+          squares[currentPosition + y * rowNumber + x].style.backgroundColor = colors[randomNumber];
+          squares[currentPosition + y * rowNumber + x].style.boxShadow = '0 0 1px black'
         }
       });
     });
   }
 
-  // undraw the Piece
+  function changeLevelBtnColor() {
+    levelButtons.forEach(button => {
+      if (button.value === level.toString()) {
+        button.style.backgroundColor = '#5649f1';
+        button.style.color = 'white';
+      } else {
+        button.style.backgroundColor = 'white';
+        button.style.color = 'gray';
+      }
+    });
+  }
+
   function undraw() {
     currentPiece.forEach((row, y) => {
       row.forEach((value, x) => {
         if (value !== 0) {
-          squares[currentPosition + y * width + x].classList.remove('tetromino');
-          squares[currentPosition + y * width + x].style.backgroundColor = '';
+          squares[currentPosition + y * rowNumber + x].classList.remove('piece');
+          squares[currentPosition + y * rowNumber + x].style.backgroundColor = '';
+          squares[currentPosition + y * rowNumber + x].style.boxShadow = ''
         }
       });
     });
   }
 
-  //assign functions to keyCodes
   function control(e) {
-    if(e.keyCode === 37) {
-      moveLeft()
-    } else if (e.keyCode === 38) {
-      rotate()
-    } else if (e.keyCode === 39) {
-      moveRight()
-    } else if (e.keyCode === 40) {
-      moveDown()
+    if (timerId) {
+      const handler = keyMap[e.keyCode];
+      if (handler) {
+        handler();
+      }
     }
   }
-  document.addEventListener('keyup', control)
 
   //move down function
   function moveDown() {
     undraw();
-    currentPosition += width;
+    currentPosition += rowNumber;
     draw();
     freeze();
   }
 
-  //freeze function
   function freeze() {
-    if (getCurrentPieceIndexes().some(index => squares[index + width].classList.contains('taken'))) {
-      getCurrentPieceIndexes().forEach(index => squares[index].classList.add('taken'));
-      // start a new piece falling
+    // Kiểm tra xem có ô nào của khối hiện tại cộng với chiều rộng (có nghĩa là ở hàng bên dưới)
+    // đã được đánh dấu là 'taken' hay không.
+    if (currentPiece.some((row, y) => {
+      return row.some((value, x) => {
+        return value !== 0 && (
+            squares[currentPosition + y * rowNumber + x + rowNumber].classList.contains('taken') ||
+            squares[currentPosition + y * rowNumber + x + rowNumber].classList.contains('block')
+        );
+      });
+    })) {
+      // If the piece hits an obstacle, end the game
+      if (getCurrentPieceIndexes().some(index => squares[index].classList.contains('obstacle'))) {
+        return gameOver();
+      }
+      // Nếu có, thì đánh dấu tất cả các ô của khối piece hiện tại là 'taken'.
+      currentPiece.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if (value !== 0) {
+            squares[currentPosition + y * rowNumber + x].classList.add('taken');
+          }
+        });
+      });
+      // Bắt đầu cho một khối mới rơi
       randomNumber = nextRandom;
       nextRandom = Math.floor(Math.random() * pieces.length);
-      currentRotation = 0;
       currentPiece = pieces[randomNumber][currentRotation];
-      currentPosition = 4;
-      draw();
-      displayShape();
-      addScore();
-      gameOver();
+      currentPosition = 4; // Reset vị trí hiện tại về vị trí ban đầu
+      displayShape(); // Hiển thị khối tiếp theo trong mini-grid
+      addScore(); // Cập nhật điểm số
+      gameOver(); // Kiểm tra xem trò chơi đã kết thúc chưa
     }
   }
 
-// Move the Piece left, unless is at the edge or there is a blockage
   function moveLeft() {
     undraw()
     const isAtLeftEdge = currentPiece.some((row, y) =>
-        row.some((value, x) => value !== 0 && (currentPosition + y * width + x) % width === 0)
+        row.some((value, x) => value !== 0 && (currentPosition + y * rowNumber + x) % rowNumber === 0)
     );
     if (!isAtLeftEdge) currentPosition -= 1;
     if (getCurrentPieceIndexes().some(index => squares[index].classList.contains('taken'))) {
@@ -222,11 +368,10 @@ window.onload = () => {
     draw()
   }
 
-  // Move the Piece right, unless is at the edge or there is a blockage
   function moveRight() {
     undraw()
     const isAtRightEdge = currentPiece.some((row, y) =>
-        row.some((value, x) => value !== 0 && (currentPosition + y * width + x) % width === width - 1)
+        row.some((value, x) => value !== 0 && (currentPosition + y * rowNumber + x) % rowNumber === rowNumber - 1)
     );
     if (!isAtRightEdge) currentPosition += 1;
     if (getCurrentPieceIndexes().some(index => squares[index].classList.contains('taken'))) {
@@ -235,21 +380,12 @@ window.onload = () => {
     draw()
   }
 
-  ///FIX ROTATION OF TETROMINOS A THE EDGE
-  function isAtRight() {
-    return current.some(index=> (currentPosition + index + 1) % width === 0)
-  }
-
-  function isAtLeft() {
-    return current.some(index=> (currentPosition + index) % width === 0)
-  }
-
   function checkRotatedPosition() {
     const isAtRightEdge = currentPiece.some((row, y) =>
-        row.some((value, x) => value !== 0 && (currentPosition + y * width + x) % width === width - 1)
+        row.some((value, x) => value !== 0 && (currentPosition + y * rowNumber + x) % rowNumber === rowNumber - 1)
     );
     const isAtLeftEdge = currentPiece.some((row, y) =>
-        row.some((value, x) => value !== 0 && (currentPosition + y * width + x) % width === 0)
+        row.some((value, x) => value !== 0 && (currentPosition + y * rowNumber + x) % rowNumber === 0)
     );
 
     if(isAtRightEdge && isAtLeftEdge) { // piece would overlap both edges, typically happens with `I` piece
@@ -267,77 +403,48 @@ window.onload = () => {
     }
   }
 
-  //rotate the tetromino
   function rotate() {
     undraw();
-    currentRotation = (currentRotation + 1) % currentPiece.length;
+    if (currentRotation === 3) {
+        currentRotation = -1;
+    }
+    currentRotation += 1;
     currentPiece = pieces[randomNumber][currentRotation];
     checkRotatedPosition();
     draw();
   }
-  /////////
 
-
-
-  //show up-next tetromino in mini-grid display
-  const displaySquares = document.querySelectorAll('.mini-grid div')
-  const displayWidth = 4
-  const displayIndex = 0
-
-
-  //the Tetrominos without rotations
-  const upNextTetrominoes = [
-    [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
-    [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino
-    [1, displayWidth, displayWidth+1, displayWidth+2], //tTetromino
-    [0, 1, displayWidth, displayWidth+1], //oTetromino
-    [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] //iTetromino
-  ]
-
-  // Display the shape in the mini-grid display
   function displayShape() {
     displaySquares.forEach(square => {
-      square.classList.remove('tetromino');
+      square.classList.remove('piece');
       square.style.backgroundColor = '';
     });
     pieces[nextRandom][0].forEach((row, y) => {
       row.forEach((value, x) => {
         if (value !== 0) {
-          displaySquares[displayIndex + y * displayWidth + x].classList.add('tetromino');
+          displaySquares[displayIndex + y * displayWidth + x].classList.add('piece');
           displaySquares[displayIndex + y * displayWidth + x].style.backgroundColor = colors[nextRandom];
         }
       });
     });
   }
 
-  //add functionality to the button
-  startBtn.addEventListener('click', () => {
-    if (timerId) {
-      clearInterval(timerId)
-      timerId = null
-    } else {
-      draw()
-      timerId = setInterval(moveDown, 1000)
-      nextRandom = Math.floor(Math.random() * pieces.length) // sửa đổi từ theTetrominoes thành pieces
-      displayShape()
-    }
-  })
-
   //add score
   function addScore() {
     let linesCleared = 0;
-    for (let i = 0; i < squares.length; i += width) {
-      const row = Array.from({ length: width }, (_, k) => i + k);
+    for (let i = 0; i < squares.length; i += rowNumber) {
+      const row = Array.from({ length: rowNumber }, (_, k) => i + k);
       if (row.every(index => squares[index].classList.contains('taken'))) {
         score += 10;
         linesCleared += 1;
         row.forEach(index => {
           squares[index].classList.remove('taken');
-          squares[index].classList.remove('tetromino');
+          squares[index].classList.remove('piece');
           squares[index].style.backgroundColor = '';
-        });
+          squares[index].style.boxShadow = ''
 
-        const squaresRemoved = squares.splice(i, width);
+        });
+        const squaresRemoved = squares.splice(i, rowNumber);
         squares = squaresRemoved.concat(squares);
         squares.forEach(cell => grid.appendChild(cell));
       }
@@ -345,29 +452,127 @@ window.onload = () => {
 
     if (linesCleared > 0) {
       scoreDisplay.innerHTML = score;
-      linesCleared = 0;
+      generateNewPiece();
     }
+
+    // Check if player has reached 50 points
+    if (score >= 50) {
+      clearInterval(timerId);
+      notification("You've won!");
+    }
+
   }
 
-  //game over
+  function generateNewPiece() {
+    // Update the reference for the next tetromino
+    randomNumber = nextRandom;
+    nextRandom = Math.floor(Math.random() * pieces.length);
+    currentPiece = pieces[randomNumber][currentRotation];
+    currentPosition = 4; // Reset current position to the start position
+    draw(); // Draw the new tetromino
+    displayShape(); // Update the next tetromino in the mini-grid
+  }
+
   function gameOver() {
-    if (getCurrentPieceIndexes().some(index => squares[index].classList.contains('taken'))) {
-      scoreDisplay.innerHTML = 'end';
+    // Kiểm tra xem liệu có phần tử nào trong khối hiện tại nằm ở vị trí đã được 'taken' hay không.
+    if (currentPiece.some((row, y) => {
+      return row.some((value, x) => {
+        return value !== 0 && squares[currentPosition + y * rowNumber + x].classList.contains('taken');
+      });
+    })) {
       clearInterval(timerId);
-      // Optional: Add a control to let the player restart the game.
+      timerId = null;
+      document.removeEventListener('keyup', control);
+      notification('Game Over')
+      newGame.addEventListener('click', restartGame);
     }
   }
 
   // trả về một mảng các index dựa trên vị trí và hướng xoay hiện tại của mảng 'Piece'
   function getCurrentPieceIndexes() {
-    let indexes = [];
+    current = [];
     currentPiece.forEach((row, y) => {
       row.forEach((value, x) => {
         if (value !== 0) {
-          indexes.push(currentPosition + y * width + x);
+          current.push(currentPosition + y * rowNumber + x);
         }
       });
     });
-    return indexes;
+    console.log(current);
+    return current;
+  }
+
+  function restartGame() {
+    // Clear the previous game state, score, and timer.
+    document.addEventListener('keyup', control);
+    scoreDisplay.innerHTML = 0;
+    score = 0;
+    startBtn.textContent = 'Start';
+
+    // Clear the grid and display.
+    squares.forEach(square => {
+      square.classList.remove('piece', 'taken');
+      square.style.backgroundColor = '';
+    });
+    displaySquares.forEach(square => {
+      square.classList.remove('piece');
+      square.style.backgroundColor = '';
+    });
+  }
+
+  function notification(noti) {
+    const notiElement = document.querySelector(".noti")
+    notiElement.textContent = noti
+    endElement.style.display = 'flex'
+  }
+
+  function generateObstacles() {
+    let obstacleIndex = 40
+    let obstacles = []
+    squares.forEach((square, index) => {
+      if (index === obstacleIndex) {
+        obstacles.push(square)
+      }
+    });
+
+    obstacles.forEach((obstacle, index) => {
+      obstacle.classList.add('obstacle');
+    })
+
+    let moveRight = true;
+
+    setInterval(() => {
+      obstacles.forEach((obstacle, index) => {
+        const obstacleIndex = squares.indexOf(obstacle);
+
+        // Remove the 'obstacle' class from the current square
+        squares[obstacleIndex].classList.remove('obstacle');
+
+        // Determine the new position of the obstacle
+        let newObstacleIndex;
+        if (moveRight) {
+          // If the obstacle is at the right edge of the grid, mark it to move left in the next move
+          if (obstacleIndex % rowNumber === rowNumber - 1) {
+            moveRight = false;
+            newObstacleIndex = obstacleIndex - 1;
+          } else {
+            newObstacleIndex = obstacleIndex + 1;
+          }
+        } else {
+          // If the obstacle is at the left edge of the grid, mark it to move right in the next move
+          if (obstacleIndex % rowNumber === 0) {
+            moveRight = true;
+            newObstacleIndex = obstacleIndex + 1;
+          } else {
+            newObstacleIndex = obstacleIndex - 1;
+          }
+        }
+
+        // Add the 'obstacle' class to the new square
+        squares[newObstacleIndex].classList.add('obstacle');
+        // Update the obstacle in the obstacles array
+        obstacles[index] = squares[newObstacleIndex];
+      });
+    }, 1000);
   }
 }
